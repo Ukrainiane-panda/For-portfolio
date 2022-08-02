28bfedb80123f811f954cc38ce5f73d4be5224b5
Vira <sheludkovira@gmail.com>
2022-08-03 00:13:39 +0300

    Editing the js file

--- vira.js
+++ vira.js
@@ -1,24 +1,24 @@
 document.querySelector("#project1").addEventListener("click", () => {
-    var template = document.querySelector("#QR1");
-    var templateClone1 = template.content.cloneNode(true);
+    var template1 = document.querySelector("#QR1");
+    var templateClone1 = template1.content.cloneNode(true);
     document.querySelector("#template-qr").append(templateClone1);
 
     });
 document.querySelector("#project2").addEventListener("click", () => {
-    var template = document.querySelector("#QR2");
-    var templateClone1 = template.content.cloneNode(true);
+    var template2 = document.querySelector("#QR2");
+    var templateClone1 = template2.content.cloneNode(true);
     document.querySelector("#template-qr").append(templateClone1);
     
     });
 document.querySelector("#project3").addEventListener("click", () => {
-    var template = document.querySelector("#QR3");
-    var templateClone1 = template.content.cloneNode(true);
+    var template3 = document.querySelector("#QR3");
+    var templateClone1 = template3.content.cloneNode(true);
     document.querySelector("#template-qr").append(templateClone1);
         
     });
 document.querySelector("#project4").addEventListener("click", () => {
-    var template = document.querySelector("#QR4");
-    var templateClone1 = template.content.cloneNode(true);
+    var template4 = document.querySelector("#QR4");
+    var templateClone1 = template4.content.cloneNode(true);
     document.querySelector("#template-qr").append(templateClone1);
             
     });
