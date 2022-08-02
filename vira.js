abb5a733a497dd79ccd3564b9efc6c580870ae37
Vira <sheludkovira@gmail.com>
2022-08-02 23:13:02 +0300

    add 4 template

--- vira.js
+++ vira.js
@@ -1,8 +1,26 @@
 document.querySelector("#project1").addEventListener("click", () => {
     var template = document.querySelector("#QR1");
     var templateClone1 = template.content.cloneNode(true);
-    document.querySelector("#template-qr1").append(templateClone1);
+    document.querySelector("#template-qr").append(templateClone1);
 
+    });
+document.querySelector("#project2").addEventListener("click", () => {
+    var template = document.querySelector("#QR2");
+    var templateClone1 = template.content.cloneNode(true);
+    document.querySelector("#template-qr").append(templateClone1);
+    
+    });
+document.querySelector("#project3").addEventListener("click", () => {
+    var template = document.querySelector("#QR3");
+    var templateClone1 = template.content.cloneNode(true);
+    document.querySelector("#template-qr").append(templateClone1);
+        
+    });
+document.querySelector("#project4").addEventListener("click", () => {
+    var template = document.querySelector("#QR4");
+    var templateClone1 = template.content.cloneNode(true);
+    document.querySelector("#template-qr").append(templateClone1);
+            
     });
 
     
