0278ae7d4cb5cb6604407ca3861b9c23b2ca1210
Vira <sheludkovira@gmail.com>
2022-08-24 17:00:48 +0300

    hobby page and audio player

--- hobby.js
+++ hobby.js
@@ -3,8 +3,8 @@ let songs = [];
 songs[0] = document.querySelector("#one");
 songs[1] = document.querySelector("#two");
 songs[2] = document.querySelector("#rthree");
-songs[3] = document.querySelector("#rthree");
-songs[4] = document.querySelector("#rthree");
+songs[3] = document.querySelector("#four");
+songs[4] = document.querySelector("#five");
 
 
 
