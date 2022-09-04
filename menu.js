30adf3190cc0d8b6bfe6c94c3b3bd40985d62104
Vira <sheludkovira@gmail.com>
2022-09-04 19:15:26 +0300

    drop down menu hobby file + added menu.js

---
+++ menu.js
@@ -0,0 +1,19 @@
+
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let menu = document.querySelector(".strila");
+    let submenu = document.querySelector("#hedder");
+
+    menu.addEventListener('click', () => {
+        if (submenu.style.display != "flex") {
+            submenu.style.display = "flex";
+
+        } else {
+            submenu.style.display = "none";
+        }
+
+    })
+
+});
